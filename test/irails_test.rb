f43e91315b1b217319b199ef5d22dffa1e8c473b
Alex Yankov <alexyankov@handybook.com>
2015-04-17 11:37:19 -0400

    Despecifing tests

--- test/irails_test.rb
+++ test/irails_test.rb
@@ -1,17 +1,21 @@
-require 'spec_helper'
+require 'test_helper'
 
-describe "render" do
-  it 'plain, no helpers or variables' do
+class IRailsTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, IRails
+  end
+
+  test 'plain, no helpers or variables' do
     out = IRails.render 'welcome/about'
     expect(out.to_s).to eq "<p>French Cooking Blog.</p>"
   end
 
-  it 'with URL helpers' do
+  test 'with URL helpers' do
     out = IRails.render 'welcome/index'
     expect(out.to_s).to eq "<h1>Welcome#index</h1>\n\n<a href=\"/articles\">My Blog</a>"
   end
 
-  it 'with a model' do
+  test 'with a model' do
     article = double("article")
     allow(article).to receive(:title).and_return("The RSpec Book")
     allow(article).to receive(:text).and_return("The RSpec Text")
